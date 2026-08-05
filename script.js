754fa432b81c59302294b3ac48293d2eee60b00e
Wuraola Adebisi <the.wuraola.adebisi@gmail.com>
2026-08-05 23:30:35 +0100

    Refine navigation, menu layout, and site branding

--- script.js
+++ script.js
@@ -5,3 +5,26 @@ document.querySelectorAll('#menu img').forEach((img) => {
     img.addEventListener('load', () => img.classList.add('is-loaded'));
   }
 });
+
+const header = document.querySelector(".main-header");
+
+window.addEventListener("scroll", () => {
+  header.classList.toggle("scrolled", window.scrollY > 20);
+});
+
+const menuButton = document.querySelector(".menu-toggle");
+const nav = document.querySelector(".nav-bar");
+
+menuButton.addEventListener("click", () => {
+  nav.classList.toggle("open");
+});
+
+document.querySelectorAll(".nav-bar a").forEach(link => {
+
+  link.addEventListener("click", () => {
+
+    nav.classList.remove("open");
+
+  });
+
+});
